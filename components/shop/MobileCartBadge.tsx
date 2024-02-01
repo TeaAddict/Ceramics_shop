e8fc7d4cc322e372ccbb920c63223d18d4292b5f
DeividasM <deividasmor7@gmail.com>
2024-02-01 21:33:24 +0200

    Add cart to mobile bar

--- components/shop/MobileCartBadge.tsx
+++ components/shop/MobileCartBadge.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import { IoCartOutline } from "react-icons/io5";
 
-const CartBadge = ({ value = 0 }: { value?: number }) => {
+const MobileCartBadge = ({ value = 0 }: { value?: number }) => {
   return (
     <Link className="flex relative p-3" href="/cart">
-      <IoCartOutline size={40} className="cursor-pointer" />
+      <IoCartOutline size={30} className="cursor-pointer" />
       {value > 0 && (
         <span className="absolute flex justify-center items-center w-[30px] h-[30px] bg-primary rounded-full bottom-7 left-7 cursor-pointer">
           <p>{value}</p>
@@ -14,4 +14,4 @@ const CartBadge = ({ value = 0 }: { value?: number }) => {
   );
 };
 
-export default CartBadge;
+export default MobileCartBadge;
--- components/shop/MobileFooter.tsx
+++ components/shop/MobileFooter.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import { FaFilter } from "react-icons/fa";
 
 import { FaSortAlphaUpAlt } from "react-icons/fa";
+import MobileCartBadge from "./MobileCartBadge";
 
 const MobileFooter = () => {
   return (
-    <div className="fixed bottom-0 left-0 bg-accent/75 w-full justify-around flex py-3 items-center">
-      <div className="bg-primary/75 rounded-full w-10 h-10 justify-center flex items-center">
-        <FaFilter className="text-foreground" size={18} />
+    <div className="fixed bottom-0 left-0 bg-accent/75 w-full grid grid-cols-3 py-3 items-center">
+      <div className="flex justify-center">
+        <div className="bg-primary/75 rounded-full w-10 h-10 justify-center flex items-center">
+          <FaFilter className="text-foreground" size={18} />
+        </div>
       </div>
-      <div className="bg-primary/75 rounded-full w-10 h-10 justify-center flex items-center">
-        <FaSortAlphaUpAlt className="text-foreground" size={30} />
+      <div className="flex justify-center">
+        <div className="bg-primary/75 rounded-full w-10 h-10 justify-center flex items-center">
+          <FaSortAlphaUpAlt className="text-foreground" size={25} />
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="bg-primary/75 rounded-full w-10 h-10 justify-center flex items-center">
+          <MobileCartBadge value={0} />
+        </div>
       </div>
     </div>
   );
