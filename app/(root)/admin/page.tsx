6cf4d98fb8677fb1895d573dd7637d784c6b4d56
DeividasM <deividasmor7@gmail.com>
2024-02-18 20:03:18 +0200

    fix: make admin page server side

--- app/(root)/admin/page.tsx
+++ app/(root)/admin/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-import VerticalMenu from "@/components/shared/VerticalMenu";
-import { ADMIN_MENU } from "@/constants";
-import React, { useState } from "react";
+import React from "react";
+import AdminVerticalMenu from "@/components/admin/AdminVerticalMenu";
 import Dashboard from "@/components/admin/Dashboard";
 import Account from "@/components/shared/Account";
 import Shopboard from "@/components/admin/Shopboard";
@@ -9,40 +7,15 @@ import Shopboard from "@/components/admin/Shopboard";
 const AdminPage = ({
   searchParams,
 }: {
-  searchParams: { category: string; sortBy: string };
+  searchParams: { tab: string; category: string; sortBy: string };
 }) => {
-  const [tab, setTab] = useState(ADMIN_MENU[0].label);
-
-  function handleChangeTab(label: string) {
-    setTab(label);
-  }
+  const { tab } = searchParams;
   return (
     <>
-      <section className="hidden padding-container sm:flex flex-col gap-5">
-        <div className="flex">
-          <div className="pt-20">
-            <VerticalMenu
-              activeValue={tab}
-              menuList={ADMIN_MENU}
-              onClick={handleChangeTab}
-            />
-          </div>
-
-          <div className=" w-full rounded-md bg-accent">
-            {tab === "dashboard" && <Dashboard />}
-            {tab === "account" && <Account />}
-            {tab === "shopboard" && <Shopboard searchParams={searchParams} />}
-          </div>
-        </div>
-      </section>
-      <section className="sm:hidden padding-container">
-        <div className="flex flex-col gap-5">
-          <div className="pt-20">
-            <VerticalMenu
-              activeValue={searchParams.category}
-              menuList={ADMIN_MENU}
-              onClick={handleChangeTab}
-            />
+      <section className="padding-container gap-5">
+        <div className="flex flex-col sm:flex-row">
+          <div className="sm:pt-20">
+            <AdminVerticalMenu />
           </div>
 
           <div className=" w-full rounded-md bg-accent">
